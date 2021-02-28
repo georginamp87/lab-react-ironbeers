be945aac9825a2ceedd50211be131fd67a005159
Georgina Morales <georginamp87@gmail.com>
2021-02-28 23:54:14 +0100

    iteration 6 done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,8 +8,50 @@ import BeerDetails from './components/BeerDetails'
 import RandomBeer from './components/RandomBeer';
 import NewBeer from './components/NewBeer';
 import {withRouter} from 'react-router-dom'
+import axios from 'axios';
 
-function App() {
+class App extends Component {
+
+  state = {
+    beers: []
+  }
+
+  componentDidMount(){
+    axios.get('https://ih-beers-api2.herokuapp.com/beers')
+      .then((response) => {
+        this.setState({ beers: response.data})
+      })
+      .catch(() => {
+        console.log('Fetching failed')
+      })
+  }
+
+  handleSubmit=(event) => {
+    event.preventDefault()
+    let name = event.target.name.value
+    let description = event.target.description.value
+    let tagline = event.target.tagline.value
+    let first_brewed = event.target.first_brewed.value
+    let brewer_tips = event.target.brewer_tips.value
+    let attenuation_level = event.target.attenuation_level.value
+    let contributed_by = event.target.contributed_by.value
+
+    axios.post('https://ih-beers-api2.herokuapp.com/beers/new', {
+    name: name, description: description, tagline: tagline, first_brewed:first_brewed, brewer_tips:brewer_tips, attenuation_level:attenuation_level, contributed_by:contributed_by})
+
+    .then((response) => {
+      this.setState({
+        beers: [response.data, ...this.state.beers]
+      }, () => {
+        this.props.history.push('/beers')
+      })
+    })
+    .catch((err) => {
+      console.log('Create failed', err)
+    })
+  }
+
+  render() {
 
   return (
     <div className="App">
@@ -19,14 +61,16 @@ function App() {
         <Route path="/beers/:beerId" render={(routeProps)=> {
           return <BeerDetails {...routeProps}/>
         }}/>
-        <Route path="/random-beer" render={(routeProps)=> {
-          return <RandomBeer {...routeProps}/>
+        <Route path="/random-beer" component={RandomBeer}/>
+        <Route path="/new-beer" render={(routeProps)=> {
+          return <NewBeer onAdd={this.handleSubmit} {...routeProps}/>
         }}/>
-        <Route path="/new-beer" component={NewBeer}/>
         </Switch>
       
     </div>
-  );
+    );
+  }
+
 }
 
-export default withRouter(App);
+export default withRouter(App)
--- src/components/NewBeer.js
+++ src/components/NewBeer.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
 import Header from './Header'
+import {Form, Button} from 'react-bootstrap'
 
 export default class NewBeer extends Component {
     render() {
         return (
             <React.Fragment>
                 <Header/>
-            <form>
-                <input type="text" name="name" placeholder="Enter name"/>
-                <input type="text" name="tagline" placeholder="Enter tagline"/>
-                <input type="text" name="description" placeholder="Enter description"/>
-                <input type="text" name="first-brewed" placeholder="Enter first brewed date"/>
-                <input type="text" name="brewer-tips" placeholder="Enter brewer tips"/>
-                <input type="number" name="attenuation-level" placeholder="Enter attenuation level"/>
-                <input type="image" name="contributed-by" placeholder="Enter name of contributor"/>
-                
-            </form>
+                <div className="container">
+                <form onSubmit={this.props.onAdd}>
+                <Form.Group controlId="formBasicEmail">
+                    <input type="text" name="name" placeholder="Enter a name"/>
+                    <input type="text" name="tagline" placeholder="Enter tagline"/>
+                    <input type="text" name="description" placeholder="Enter description"/>
+                    <input type="text" name="first_brewed" placeholder="Enter first brewed date"/>
+                    <input type="text" name="brewer_tips" placeholder="Enter brewer tips"/>
+                    <input type="number" name="attenuation_level" placeholder="Enter attenuation level"/>
+                    <input type="text" name="contributed_by" placeholder="Enter name of contributor"/>
+                    <button variant="info" type="submit">Create New Beer!</button>
+                    </Form.Group>
+                </form>
+            </div>
             </React.Fragment>
         )
     }
